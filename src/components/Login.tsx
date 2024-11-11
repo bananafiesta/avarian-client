dd6096f68f5f47de800171557c97863f7174bd4a
bananafiesta <zhang.vctr@gmail.com>
2024-11-11 03:29:24 -0800

    add reminder for future authentication changes

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -36,6 +36,8 @@ export function Login(): ReactNode {
       await supabase.auth.signInWithOAuth({
         provider: 'discord',
         options: {
+          // redirect to handle backend
+          // redirectTo: process.env.URL_TO_USE + '/auth/callback'
           redirectTo: prevUrl
         }
       }).then(() => {
