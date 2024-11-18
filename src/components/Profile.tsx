323ee6bc0cd32ca2b4d386356320a44d338861ed
bananafiesta <zhang.vctr@gmail.com>
2024-11-18 09:28:12 -0800

    remove unused import from profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useState, useEffect } from "react";
+import { ReactElement, useState, useEffect } from "react";
 import { supabase } from "./supabase";
 
 interface economy_obj {
