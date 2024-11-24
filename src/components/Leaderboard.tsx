631c2b644ad33afdefadb09110dca4ce64d391cb
bananafiesta <zhang.vctr@gmail.com>
2024-11-24 15:43:55 -0800

    update dropdown menu for profile and leaderboard

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,5 +1,5 @@
 import { ReactElement, useEffect, useState } from "react";
-import { Select } from "@headlessui/react";
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from "@headlessui/react";
 import { useQueries, useQuery, UseQueryResult } from "@tanstack/react-query";
 import { fetchName } from "../utils/fetching";
 
@@ -34,7 +34,7 @@ async function fetchLeaderboard(skill: string) {
 function LoadingScreen(): ReactElement {
   return (
     <div className="flex grow justify-center items-center">
-      <svg className="animate-spin size-16" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <svg className="animate-spin size-24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
         <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
         <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
       </svg>
@@ -71,7 +71,7 @@ function LeaderboardTable({leaderboardMap}: {leaderboardMap: Map<string, UseQuer
   }
   return (
     <>
-    <div className="flex flex-row w-2/3 justify-between mt-1 font-dosis">
+    <div className="flex flex-row w-2/3 justify-between mt-1">
       <p className="px-5 self-end text-xl font-medium">Leaderboard</p>
       <LeaderboardDropdown skill={skill} setSkill={setSkill} />
     </div>
@@ -98,14 +98,14 @@ function LeaderboardLoader({currentQuery}: {currentQuery: UseQueryResult}): Reac
     <div className="overflow-y-auto h-screen">
       <table className="table-auto border-separate place-items-center border-spacing-x-5 border-spacing-y-4">
         <thead className="">
-          <tr className="font-dosis">
+          <tr className="">
             <th className="">Rank</th>
             <th className=""></th>
             <th className="">Name</th>
             <th className="">Levels</th>
           </tr>
         </thead>
-        <tbody className="text-lg font-dosis font-semibold text-center">
+        <tbody className="text-lg font-semibold text-center">
           {entries}
         </tbody>
       </table>
@@ -115,20 +115,25 @@ function LeaderboardLoader({currentQuery}: {currentQuery: UseQueryResult}): Reac
 
 function LeaderboardDropdown({skill, setSkill}: {skill: string, setSkill: (arg0: string) => void}): ReactElement {
   return (
-    <Select value={skill} onChange={option => setSkill(option.target.value)} className="mr-4 text-center font-medium rounded-full bg-gray-100/60 data-[hover]:bg-gray-100/90 w-2/5">
-      <option value="total">Total</option>
-      <option value="agility">Agility</option>
-      <option value="alchemy">Alchemy</option>
-      <option value="archery">Archery</option>
-      <option value="defense">Defense</option>
-      <option value="enchanting">Enchanting</option>
-      <option value="excavation">Excavation</option>
-      <option value="farming">Farming</option>
-      <option value="fighting">Fighting</option>
-      <option value="fishing">Fishing</option>
-      <option value="foraging">Foraging</option>
-      <option value="mining">Mining</option>
-    </Select>
+    <Listbox value={skill} onChange={setSkill}>
+      <ListboxButton className="bg-gray-700/70 rounded-xl mb-1 flex flex-row justify-between font-nunito text-white items-center px-2 text-lg hover:bg-gray-700/50 w-2/5">
+        {skill.charAt(0).toUpperCase() + skill.slice(1)}
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+        </svg>
+      </ListboxButton>
+      <ListboxOptions transition anchor="bottom start" className="w-[var(--button-width)] border border-gray-500/5 [--anchor-gap:2px] rounded-xl transition ease-in duration-150 data-[closed]:opacity-0 bg-gray-600 p-1">
+        <ListboxOption key={"total"} value={"total"} className="flex flex-row font-nunito text-white items-center gap-4 px-2 data-[focus]:bg-white/10 rounded-xl">
+          Total
+        </ListboxOption>
+        {skills.map((currSkill) => (
+          <ListboxOption key={currSkill} value={currSkill} className="flex flex-row font-nunito text-white items-center gap-4 px-2 data-[focus]:bg-white/10 rounded-xl">
+            {currSkill.charAt(0).toUpperCase() + currSkill.slice(1)}
+          </ListboxOption>
+        ))}
+      </ListboxOptions>
+    </Listbox>
+ 
   )
 }
 
@@ -152,7 +157,7 @@ export function Leaderboard(): ReactElement {
 
   return (
     <div className="flex justify-center items-center grow bg-[url('/mc_home.png')] bg-cover bg-center relative">
-      <div className="bg-gray-300/50 mb-1 rounded-lg flex flex-col items-center absolute top-[2%] bottom-[5%] px-5 md:w-1/3 min-w-80">
+      <div className="bg-gray-700/50 text-white font-nunito mb-1 rounded-lg flex flex-col items-center absolute top-[2%] bottom-[5%] px-5 md:w-1/3 min-w-80">
         <LeaderboardTable leaderboardMap={leaderboardMap} />
       </div>
     </div>
--- src/components/ProfilePage.tsx
+++ src/components/ProfilePage.tsx
@@ -47,10 +47,12 @@ async function fetchProfiles(accessToken: string): Promise<Array<profileData>> {
 function LoadingScreen(): ReactElement {
   return (
     <div className="flex grow justify-center items-center">
-      <svg className="animate-spin size-16" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-      </svg>
+      <div className="bg-gray-600/70 text-white p-4 rounded-xl">
+        <svg className="animate-spin size-24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+      </div>
     </div>
   )
 }
@@ -154,7 +156,9 @@ function StatusSwitch({currentStatus, profiles}: {currentStatus: status, profile
   const [uuid, setUuid] = useState("");
   switch (currentStatus) {
     case status.Loading:
-      return <LoadingScreen />
+      return (
+        <LoadingScreen />
+      )
     case status.Done:
       if (index == -1 || index >= profiles.length || profiles[index].uuid !== uuid) {
         setIndex(0);
